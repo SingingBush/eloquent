1143a13e8d60890815f330798cf78777afcfec05
SingingBush <singingbush@hotmail.com>
2016-08-20 15:46:55 +0100

    no longer try to load app.properties when running tests

--- source/config/properties.d
+++ source/config/properties.d
@@ -9,11 +9,16 @@ class Properties {
     private string[string] _properties;
 
     public this() {
-        string filePath = "./app.properties";
-        readOption("p|properties", &filePath, "path to properites file. Defaults to './app.properties'");
+    	version(DEVELOPMENT) {
+    		logInfo("Properties -> test properties");
+    		_properties["db.file"] = "schema.sql";
+    	} else {
+    		string filePath = "./app.properties";
+			readOption("p|properties", &filePath, "path to properites file. Defaults to './app.properties'");
 
-        logInfo("Properties -> loading properties file: '%s'", filePath);
-        _properties = readProperties(filePath);
+			logInfo("Properties -> loading properties file: '%s'", filePath);
+			_properties = readProperties(filePath);
+    	}
     }
 
     T as(T)(string name, T alternative=T.init) {
